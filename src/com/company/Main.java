21392ba0cc8819a5c423da7c0f95ad7d37efc9b1
LadyGaga <lady.gagalamasguapa@gmail.com>
2020-12-11 14:25:32 +0100

    Añadido Ya queda menos en Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Quiero que llegen las vacaciones");
+        System.out.println("Ya queda menos");
     }
 }
